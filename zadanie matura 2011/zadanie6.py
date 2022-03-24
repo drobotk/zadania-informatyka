e3d409f5a0063310d800b9afcd8dc2c134ac2754
drobotk <pawwwll@gmail.com>
2022-03-24 17:37:34 +0100

    Update zadanie6

--- zadanie matura 2011/zadanie6.py
+++ zadanie matura 2011/zadanie6.py
@@ -1,18 +1,25 @@
 def main():
     with open("./Dane/liczby.txt") as f:
         lines = f.readlines()
-        
+
     strs = [x.strip() for x in lines]
     numbers = [int(x, 2) for x in strs]
-        
+
     even = len([x for x in numbers if x % 2 == 0])
     biggest = max(numbers)
     niners = [int(x, 2) for x in strs if len(x) == 9]
 
-    with open("./zadanie6.txt", "w") as f:
-        f.write(f"a) {even}\n")
-        f.write(f"b) {biggest}, {bin(biggest)}\n")
-        f.write(f"c) {len(niners)}, {bin(sum(niners))}\n")
+    results = (
+        f"a) {even}\n"
+        f"b) {biggest}, {bin(biggest)[2:]}\n"
+        f"c) {len(niners)}, {bin(sum(niners))[2:]}\n"
+    )
+
+    print(results)
     
+    with open("./zadanie6.txt", "w") as f:
+        f.write(results)
+
+
 if __name__ == "__main__":
     main()
