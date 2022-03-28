f69e11dcd5b84eb61b196dce69a6720df85024f2
Dawid K <80264606+drobotk@users.noreply.github.com>
2022-03-28 14:48:50 +0200

    Add files via upload

---
+++ zadanie matura 2013/zadanie6.py
@@ -0,0 +1,26 @@
+def main():
+    with open("./Dane/dane.txt") as f:
+        strs = [x.strip() for x in f.readlines()]
+
+    first_last = [x for x in strs if x[0] == x[-1]]
+
+    decimal = [str(int(x, 8)) for x in strs]
+    first_last_decimal = [x for x in decimal if x[0] == x[-1]]
+
+    ascending = 0
+    for x in strs:
+        if [*x] == sorted([*x]):
+            ascending += 1
+
+    results = (
+        f"a) {len(first_last)}\n" f"b) {len(first_last_decimal)}\n" f"c) {ascending}\n"
+    )
+
+    print(results)
+
+    with open("./wyniki6.txt", "w") as f:
+        f.write(results)
+
+
+if __name__ == "__main__":
+    main()
